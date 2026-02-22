369726d513b50d98014efdceef3491e2039caa62
Dekoder-py <dekod3r.py@gmail.com>
2026-02-23 09:36:01 +1300

    Add basic player movement

---
+++ scripts/player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+
+func _physics_process(delta: float) -> void:
+	# Get the input direction and handle the movement/deceleration.
+	var x_direction := Input.get_axis("left", "right")
+	if x_direction:
+		velocity.x = x_direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	var y_direction := Input.get_axis("up", "down")
+	if y_direction:
+		velocity.y = y_direction * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
